932f764d8a27df78c0870c2a145f1b32c6db03e9
Pip Matthews <pip.matthews+github@gmail.com>
2023-03-09 19:58:56 -0500

    creating list of cards

--- Program.cs
+++ Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeck
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //Create the deck of cards as a list of strings
+            var cards = new List<string>() { "King of Hearts" };
+
+            cards.Add("Queen of Hearts");
+            cards.Add("Jack of Hearts");
+            cards.Add("10 of Hearts");
+            cards.Add("9 of Hearts");
+            cards.Add("8 of Hearts");
+            cards.Add("7 of Hearts");
+            cards.Add("6 of Hearts");
+            cards.Add("5 of Hearts");
+            cards.Add("4 of Hearts");
+            cards.Add("3 of Hearts");
+            cards.Add("2 of Hearts");
+            cards.Add("Ace of Hearts");
+            cards.Add("King of Spades");
+            cards.Add("Queen of Spades");
+            cards.Add("Jack of Spades");
+            cards.Add("10 of Spades");
+            cards.Add("9 of Spades");
+            cards.Add("8 of Spades");
+            cards.Add("7 of Spades");
+            cards.Add("6 of Spades");
+            cards.Add("5 of Spades");
+            cards.Add("4 of Spades");
+            cards.Add("3 of Spades");
+            cards.Add("2 of Spades");
+            cards.Add("Ace of Spades");
+            cards.Add("King of Diamonds");
+            cards.Add("Queen of Diamonds");
+            cards.Add("Jack of Diamonds");
+            cards.Add("10 of Diamonds");
+            cards.Add("9 of Diamonds");
+            cards.Add("8 of Diamonds");
+            cards.Add("7 of Diamonds");
+            cards.Add("6 of Diamonds");
+            cards.Add("5 of Diamonds");
+            cards.Add("4 of Diamonds");
+            cards.Add("3 of Diamonds");
+            cards.Add("2 of Diamonds");
+            cards.Add("Ace of Diamonds");
+            cards.Add("King of Clubs");
+            cards.Add("Queen of Clubs");
+            cards.Add("Jack of Clubs");
+            cards.Add("10 of Clubs");
+            cards.Add("9 of Clubs");
+            cards.Add("8 of Clubs");
+            cards.Add("7 of Clubs");
+            cards.Add("6 of Clubs");
+            cards.Add("5 of Clubs");
+            cards.Add("4 of Clubs");
+            cards.Add("3 of Clubs");
+            cards.Add("2 of Clubs");
+            cards.Add("Ace of Clubs");
+
+            //shuffle the cards using youtube method
+
+
+            //display top two cards
+
+
+        }
     }
-  }
 }
